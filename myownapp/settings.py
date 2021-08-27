8f7f4d84a7a08be75f6815f7340518da2ded0643
Dominik Kozaczko <dominik@kozaczko.info>
2021-08-27 22:20:27 +0200

    Część 5

--- myownapp/settings.py
+++ myownapp/settings.py
@@ -130,4 +130,5 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
     "DEFAULT_PAGINATION_CLASS": "rental.pagination.HeaderLimitOffsetPagination",
     "PAGE_SIZE": 100,
+    "DEFAULT_FILTER_BACKENDS": ("django_filters.rest_framework.DjangoFilterBackend",),
 }
--- rental/api_views.py
+++ rental/api_views.py
@@ -1,3 +1,5 @@
+import django_filters
+import pendulum
 from rest_framework import viewsets
 
 from . import models, serializers
@@ -15,6 +17,21 @@ class BelongingViewSet(viewsets.ModelViewSet):
     permission_classes = [IsOwner]
 
 
+class LoanFilterSet(django_filters.FilterSet):
+    missing = django_filters.BooleanFilter(field_name="returned", lookup_expr="isnull")
+    overdue = django_filters.BooleanFilter(method="get_overdue", field_name="returned")
+
+    class Meta:
+        model = models.Loan
+        fields = ["what", "to_who", "missing", "overdue"]
+
+    def get_overdue(self, queryset, field_name, value):
+        if value:
+            return queryset.filter(when__lte=pendulum.now().subtract(months=2))
+        return queryset
+
+
 class LoanViewSet(viewsets.ModelViewSet):
     queryset = models.Loan.objects.all()
     serializer_class = serializers.LoanSerializer
+    filterset_class = LoanFilterSet
